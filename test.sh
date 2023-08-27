3337ea2584977de76381119894147a04a0db8dd7
jijizhang <44020726+zhangjijin@users.noreply.github.com>
2023-08-27 21:05:42 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,3 +6,8 @@ nohup samtools view -h CS_sg_2017_30x.bam | extractSplitReads_BwaMem -i stdin | 
 nohup samtools view -b -F 1294  CS_sg_2017_30x.bam > cs30x.discordants.unsorted.bam &
 nohup samtools sort -@20 -o cs30x.discordants.bam  cs30x.discordants.unsorted.bam &
 nohup samtools sort -@20 -o cs30x.splitters.bam  cs30x.splitters.unsorted.bam &
+
+nohup mosdepth -n --fast-mode -T 20  --by 500 cs_30x.discordants cs30x.discordants.bam  &
+nohup mosdepth -n --fast-mode -T 20 --by 500 cs_30x.splitters cs30x.splitters.bam  &
+
+paste  cs_30x.regions.bed <(cut -f4  cs_30x.splitters.regions.bed) <(cut -f4 cs_30x.discordants.regions.bed) > cs.depth.bed
